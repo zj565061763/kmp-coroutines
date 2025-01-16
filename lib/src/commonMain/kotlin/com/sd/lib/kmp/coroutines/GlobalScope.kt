a67dc581d823ac8d7b843d68ce09543803b27be8
zjun <565061763@qq.com>
2025-01-16 22:57:45 +0800

    FGlobalScope

--- lib/src/commonMain/kotlin/com/sd/lib/kmp/coroutines/GlobalScope.kt
+++ lib/src/commonMain/kotlin/com/sd/lib/kmp/coroutines/GlobalScope.kt
@@ -5,14 +5,13 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
-private val FGlobalScope = CoroutineScope(
-  SupervisorJob() + Dispatchers.Main + CoroutineName("FGlobalScope")
-)
+private val FGlobalScope = MainScope() + CoroutineName("FGlobalScope")
 
 /**
  * 全局协程，默认调度器[Dispatchers.Main]
