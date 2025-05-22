4b65b09086aefb56e0bb1b9fcc816af298140079
zjun <565061763@qq.com>
2025-05-22 10:16:52 +0800

    Revert "FStateViewModel"
    This reverts commit 7be4498772da3d772dded2f258ed95b46fbe4309.

--- lib/src/commonMain/kotlin/com/sd/lib/kmp/coroutines/GlobalScope.kt
+++ lib/src/commonMain/kotlin/com/sd/lib/kmp/coroutines/GlobalScope.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.plus
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
-val FGlobalScope = MainScope() + CoroutineName("FGlobalScope")
+private val FGlobalScope = MainScope() + CoroutineName("FGlobalScope")
 
 /**
  * 全局协程，默认调度器[Dispatchers.Main]
